32985c8faa6cabcc85ca1465d45ea384d1d897b5
Amin Jafarlou <jafarlou.amin@gmail.com>
2021-10-25 12:00:54 +0330

    object can be extracted and manipulated, now add Google Translate Api

--- index.js
+++ index.js
@@ -9,17 +9,41 @@ const initialObject = fs.readFileSync('./fa.js', 'utf8', (err, data) => {
   return data;
 });
 
-const extractedObject = initialObject.slice(
-  initialObject.indexOf('{'),
-  initialObject.indexOf('}') + 1
-);
-console.log(extractedObject, typeof extractedObject);
+const extractedObjectInString = initialObject
+  .slice(initialObject.indexOf('{'), initialObject.indexOf('}') + 1)
+  .replace(/^"(.*)"$/, '$1');
 
-const finalObject = initialObject.replace('fa', 'en');
-
-fs.writeFile('./en.js', finalObject, (err) => {
-  if (err) {
-    console.log(err);
-    return;
+const getObjectFromString = (stringToParse) => {
+  if (typeof stringToParse === 'string') {
+    let currentKey = '';
+    const keyValPairArr = stringToParse
+      .replace('{', '')
+      .replace('}', '')
+      .split(':');
+    return keyValPairArr.reduce((obj, current, index, arr) => {
+      const previousKey = currentKey;
+      const arrKeyVal = current.trim().split(',');
+      currentKey = index !== arr.length - 1 ? arrKeyVal.pop().trim() : '';
+      const previousVal = arrKeyVal.join(',');
+      if (previousKey && previousVal !== '') obj[previousKey] = previousVal;
+      return obj;
+    }, {});
+  } else {
+    return stringToParse || {};
   }
+};
+
+const extractedObj = getObjectFromString(extractedObjectInString);
+
+const results = {};
+Object.keys(extractedObj).forEach((key) => {
+  results[key] = 'XXX';
 });
+console.log(results);
+// const finalObject = initialObject.replace('fa', 'en');
+// fs.writeFile('./en.js', finalObject, (err) => {
+//   if (err) {
+//     console.log(err);
+//     return;
+//   }
+// });
